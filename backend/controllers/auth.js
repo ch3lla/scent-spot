7f09ccd6a9ea0e320dc47375324a0b7312c9822d
ch3lla <chukwumaemmanuella03@gmail.com>
2024-02-13 18:03:24 +0100

    controllers

---
+++ backend/controllers/auth.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    const userInfo = req.body;
+
+    const alreadyExists = await User.findOne({email: req.body.email});
+    if (alreadyExists){
+        res.status(400).json({message: "This email belongs to an account."});
+        return;
+    }
+
+    try {
+        if (typeof userInfo !== "object" || Object.keys(userInfo).length === 0) {
+            res.status(400).json({ message: "Please fill all the required fields" });
+            return;
+        }
+        const newUser = new User(userInfo)
+        const token = await newUser.generateAuthToken();
+        await newUser.save();
+        res.status(201).json({token:token});
+    } catch (Error){
+        console.error(Error);
+        res.status(500).json({Error: Error._message});
+    }
+});
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findByCredentials(
+            req.body.email,
+            req.body.password
+        );
+        if (!user){
+            res.status(400).send(user.error);
+            return;
+        }
+        const token = await user.generateAuthToken();
+        res.status(200).json({token});
+    } catch (error){
+        if (error.message === "This email has not been registered on our system.") {
+            res.status(400).json({ error: error.message });
+        } else if (error.message === "Invalid password") {
+            res.status(400).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: "An unexpected error occurred." });
+        }
+    }
+});
+
+module.exports = router;
