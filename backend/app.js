d06c56b4aee47342b518c38e573038cc1a1ec381
ch3lla <chukwumaemmanuella03@gmail.com>
2024-02-21 22:52:26 +0100

    changed routing structure, added a routes folder

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,25 @@
-require("dotenv").config();
-const express = require("express");
-const { json, urlencoded } = require("express");
-const cors = require("cors");
-const db = require("./config/db");
+require('dotenv').config();
+const express = require('express');
+const { json, urlencoded } = require('express');
+const cors = require('cors');
+const db = require('./config/db');
 
-const authRoute = require('./controllers/auth');
-const userRoute = require('./controllers/users.controllers');
-const productRoute = require('./controllers/product.controller');
-const cartRoute = require('./controllers/cart.controller');
+const apiRoutes = require('./routes/index');
 
 const app = express();
-//connecting database
+// connecting database
 db();
 
-//middlewares
+// middlewares
 app.use(cors());
-app.options(`${process.env.BASE_URL}`, cors()); // * will be changed to specified url later on
+app.options(`${process.env.FRONTEND_URL}`, cors()); // * will be changed to specified url later on
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-//routes
-app.use("/api/auth", authRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/product", productRoute);
-app.use("/api/v1/cart", cartRoute);
-
+// routes
+app.use('/api', apiRoutes);
 
 app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Serever is running on localhost:${process.env.PORT}`);
 });
